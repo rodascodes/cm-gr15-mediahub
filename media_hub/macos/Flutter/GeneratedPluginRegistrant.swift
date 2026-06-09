3c1fea9b3ded8df8d36ab7556648232e7370b11f
202100642 <202100642@estudantes.ips.pt>
2026-06-09 13:55:30 +0100

    small update

--- media_hub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ media_hub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,5 +13,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-
 }
